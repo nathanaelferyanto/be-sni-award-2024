276b1d5cdc88e9f57c635a400e7256b1dc12b301
lafathalfath <iqnaraidan12@gmail.com>
2024-02-20 14:39:26 +0700

    benerinlogin page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="{{ asset('assets') }}/bootstrap-5.3.2/css/bootstrap.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+      crossorigin="anonymous"
+    />
 
     <!-- Arimo Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -14,6 +19,7 @@
       href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&display=swap"
       rel="stylesheet"
     />
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/styles.css" />
 
     <!-- Josefin Sans Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -31,13 +37,13 @@
   </head>
   <body>
     <section id="login">
-      <div
-        class="login-container row align-items-center justify-content-center"
-      >
-        <div class="content-kiri col-lg-7">
+      <div class="login-container">
+
+        <div class="content-kiri">
           <img src="{{ asset('assets') }}/images/icon/Frame.svg" alt="" />
         </div>
-        <div class="content-kanan col-lg-5 py-5">
+
+        <div class="content-kanan py-5">
           <img src="{{ asset('assets') }}/images/icon/User_circle.svg" alt="" />
           <h1>SNI AWARD</h1>
           <form>
@@ -69,6 +75,7 @@
             </div>
           </form>
         </div>
+
       </div>
     </section>
   </body>
