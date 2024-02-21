9425fc38eece58cb2ae4d0698dfb2a2b85d4f70c
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-02-21 16:26:58 +0700

    CREATE : Controller Wilayah

---
+++ database/migrations/2024_02_20_071817_create_kecamatan_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kecamatan', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->tinyInteger('kota_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('kota_id')->references('id')->on('kota');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kecamatan');
+    }
+};
